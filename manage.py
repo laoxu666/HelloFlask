e211e9e87606fc9af1b13257942e7a96eb3bbed0
laoxu <xsmlxh666@163.com>
2018-07-19 16:27:13 +0800

    flask 路由参数接受

--- manage.py
+++ manage.py
@@ -1,3 +1,5 @@
+import uuid
+
 from flask import Flask, render_template
 # 安装驱动 pip install flask-script
 from flask_script import Manager
@@ -30,14 +32,19 @@ def temp():
 #string 请求参数中不能带斜线/  默认为路径分隔符
 # @app.route('/student/<string:id>/')
 # @app.route('/student/<int:id>/')
-@app.route('/student/<path:id>/')
+# @app.route('/student/<path:id>/')
 # http://127.0.0.1:5000/student/100aaa/hehe/
-
+@app.route('/student/<uuid:id>/')
 def student(id):
     print(type(id))
     print(id)
     return "有趣的参数"
 
+@app.route('/getuuid/')
+def get_uuid():
+    return str(uuid.uuid4())
+
+# d8892d52-ffce-408c-9d55-59ee5cf1e448
 
 if __name__ == '__main__':
     # app.run(debug=True,port=8000,host='0.0.0.0')
